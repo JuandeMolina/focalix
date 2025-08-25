7949115727b63bc963b47e661780028d524bfd4e
JuandeMolina <molinaperalesjd@gmail.com>
2025-08-25 17:18:35 +0200

    Reorganise app structure

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from .models import Task
+from . import db
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def home():
+    tasks = Task.query.all()
+    return render_template("base.html", tasks=tasks)
