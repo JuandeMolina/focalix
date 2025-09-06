d9e8f7ef71d3afe1003912204822a0e2a789974b
JuandeMolina <molinaperalesjd@gmail.com>
2025-09-06 16:39:35 +0200

    Fix bugs on deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
@@ -7,9 +8,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://postgres:[es4tSz^x8@6w$M78XcD#U5]@db.mooiuymcykajxptpvinx.supabase.co:5432/postgres"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
